21c70c6984e65818fbf8e3b4cee6eb35b5fb54ec
dashidan <2903872229@qq.com>
2017-12-27 14:51:04 +0800

    修改标题排序

--- dist/sinbad_showdown.js
+++ dist/sinbad_showdown.js
@@ -2649,8 +2649,27 @@ showdown.subParser('pageTitle', function (text, options, globals) {
   array.sort(function (a, b) {
                var index0 = a.split(" ")[0];
                var index1 = b.split(" ")[0];
-               /** 校正小数1.2, 1.21的大小比较*/
-               return Number(index0) * 1000 > Number(index1) * 1000 ? 1 : -1;
+               /** 校正小数1.3，1.21  的大小比较, 1.21应该在1.3的后面*/
+
+               var arr0 = index0.split(".");
+               var arr1 = index1.split(".");
+
+               var count0 = 0;
+               var count1 = 0;
+
+               if (arr0.length === 1) {
+                 count0 = Number(arr0[0]) * 100;
+               } else if (arr0.length === 2) {
+                 count0 = Number(arr0[0]) * 100 + Number(arr0[1]);
+               }
+
+               if (arr1.length === 1) {
+                 count1 = Number(arr1[0]) * 100;
+               } else if (arr1.length === 2) {
+                 count1 = Number(arr1[0]) * 100 + Number(arr1[1]);
+               }
+
+               return count0 * 1000 > Number(count1) * 1000 ? 1 : -1;
              }
   );
   return array;
